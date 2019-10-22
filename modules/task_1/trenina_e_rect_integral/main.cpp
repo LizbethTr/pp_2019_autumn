af28846b142935def79d4cb82c2ed34d6ac05e87
LizbethTr <elrtr@yandex.ru>
2019-10-22 17:55:41 +0300

    Implemented methods

--- modules/task_1/trenina_e_rect_integral/main.cpp
+++ modules/task_1/trenina_e_rect_integral/main.cpp
@@ -51,45 +51,45 @@ TEST(Rect_Intg, Test_L_Parallel_Quadratic) {
 }
 
 TEST(Rect_Intg, Test_Midpoint_Sequential_Linear) {
-	int rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	if (rank == 0) {
-		double res = get_midpoint_intg_sequential(0.0, 5.0, 1000, linear);
-		double exp = 12.5;
-		EXPECT_NEAR(res, exp, 5.2e-6); // 0.000005);
-	}
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    if (rank == 0) {
+        double res = get_midpoint_intg_sequential(0.0, 5.0, 1000, linear);
+        double exp = 12.5;
+        EXPECT_NEAR(res, exp, 5.2e-6); // 0.000005);
+    }
 }
 
 TEST(Rect_Intg, Test_Midpoint_Sequential_Quadratic) {
-	int rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	if (rank == 0) {
-		double res = get_midpoint_intg_sequential(0.0, 10.0, 1000, quadratic);
-		double exp = 333.33333;
-		EXPECT_NEAR(res, exp, 8.4e-5);
-	}
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    if (rank == 0) {
+        double res = get_midpoint_intg_sequential(0.0, 10.0, 1000, quadratic);
+        double exp = 333.33333;
+        EXPECT_NEAR(res, exp, 8.4e-5);
+    }
 }
 
 TEST(Rect_Intg, Test_Midpoint_Parallel_Linear) {
-	int rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-	double res = get_midpoint_intg_parallel(0.0, 5.0, 1000, linear);
-	if (rank == 0) {
-		double exp = get_midpoint_intg_sequential(0.0, 5.0, 1000, linear);
-		EXPECT_NEAR(res, exp, 0.4e-5);
-	}
+    double res = get_midpoint_intg_parallel(0.0, 5.0, 1000, linear);
+    if (rank == 0) {
+        double exp = get_midpoint_intg_sequential(0.0, 5.0, 1000, linear);
+        EXPECT_NEAR(res, exp, 0.4e-5);
+    }
 }
 
 TEST(Rect_Intg, Test_Midpoint_Parallel_Quadratic) {
-	int rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-	double res = get_midpoint_intg_parallel(0.0, 10.0, 1000, quadratic);
-	if (rank == 0) {
-		double exp = get_midpoint_intg_sequential(0.0, 10.0, 1000, quadratic);
-		EXPECT_NEAR(res, exp, 8.33e-5);
-	}
+    double res = get_midpoint_intg_parallel(0.0, 10.0, 1000, quadratic);
+    if (rank == 0) {
+        double exp = get_midpoint_intg_sequential(0.0, 10.0, 1000, quadratic);
+        EXPECT_NEAR(res, exp, 8.33e-5);
+    }
 }
 
 
